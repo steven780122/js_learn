e27653fc131e50646bdaffd3ceef84658de1ca60
Steven Hsiao <stevenhsiao@starlux-airlines.com>
2017-12-05 11:09:00 +0800

    Update game (not finish! (check phone photo))

--- demo16_GAME!/www/lib.js
+++ demo16_GAME!/www/lib.js
@@ -1,3 +1,103 @@
+ga.lib.BulletSystem = function(){
+    this.bullets = [];
+
+    this.update = function(){
+
+        // 放bullet:
+        // 每3回合才放子彈，讓子彈變少
+        if(ga.data.circle % 30 == 0){
+            this.bullets.push(new ga.lib.Bullet());
+        }
+        
+
+        for(let i = 0; i < this.bullets.length; i++){
+            if(this.bullets[i].update()){    // 如果死掉清除! (超出版面)
+                this.bullets.splice(i, 1);
+                i--;
+            }
+        }
+    };
+
+
+    this.draw = function(){
+        for(let i = 0; i < this.bullets.length; i++){
+            // if(this.bullets[i].update()){
+            //     console.log("TEST");
+                this.bullets[i].draw();
+            // }
+        }
+    };
+}
+
+
+
+ga.lib.Bullet = function(){
+    // 方向可以用random 去定義4個方向!   My:
+    this.random = Math.random();
+
+    if(this.random > 0.75){   // 左右     
+        this.x = 0;
+        this.y = Math.random() * ga.ctx.canvas.height;
+        this.vx = Math.random() * 2 + 1;
+        this.vy = 0;
+
+    }else if(this.random > 0.5){   // 右左
+        this.x = ga.ctx.canvas.width;
+        this.y = Math.random() * ga.ctx.canvas.height;
+        this.vx = Math.random() * 2 -3;
+        this.vy = 0;
+    }else if(this.random > 0.25){   // 上下
+        this.x = Math.random() * ga.ctx.canvas.width;
+        this.y = 0;
+        this.vx = 0;
+        this.vy = Math.random() * 2 + 1;
+
+    }else{
+        this.x = Math.random() * ga.ctx.canvas.width;  // 下上
+        this.y = ga.ctx.canvas.height;
+        this.vx = 0;
+        this.vy = Math.random() * 2 - 3;
+
+    }
+
+    this.size = 1;
+
+    // this.x = 0;
+    // this.y = Math.random() * ga.ctx.canvas.height;
+    // this.vx = Math.random() * 2 + 1;
+    // this.vy = 0;
+    // this.size = 1;
+
+
+
+
+    this.update = function(){
+        this.x += this.vx;
+        this.y += this.vy;
+
+        return this.x > ga.ctx.canvas.width || this.x < 0 || this.y > ga.ctx.canvas.height || this.y < 0;
+
+    };
+
+
+
+
+
+    this.draw = function(){
+
+        ga.ctx.save();   //儲存當下設定
+        ga.ctx.fillStyle='white';
+        
+        ga.ctx.beginPath();// 畫路徑
+        ga.ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ga.ctx.fill();
+        ga.ctx.restore();   //恢復設定
+    }
+
+}
+
+
+
 ga.lib.Plane = function(){
     this.x = ga.ctx.canvas.width /2;   // 從畫面的中間開始
     this.y = ga.ctx.canvas.height /2;
